f28fa312a9ffca59bc338d1c57218e83c163ce33
agyen <izagyen96@gmail.com>
2019-11-18 17:59:30 +0000

    update

--- ATM Dispenser/src/io/turntabl/test/AtmDispenseChain.java
+++ ATM Dispenser/src/io/turntabl/test/AtmDispenseChain.java
@@ -29,13 +29,18 @@ public class AtmDispenseChain {
             int amount = 0;
             System.out.println("Enter amount to dispense");
             Scanner input = new Scanner(System.in);
-            amount = input.nextInt();
-//            if (amount % 10 != 0) {
-//                System.out.println("Amount should be in multiple of 10s.");
-//                return;
-//            }
-            // process the request
-            atmDispenser.c1.dispense(new Currency(amount));
+
+          if (input.hasNextInt()){
+//              value is int
+              amount = input.nextInt();
+              atmDispenser.c1.dispense(new Currency(amount));
+
+          }
+          else {
+//              value is not int
+              System.out.println("Coins not allowed...!");
+          }
+
         }
 
     }
